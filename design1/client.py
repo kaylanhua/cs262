102bed6fd18f9fc674ae311d9dbb8372ab79ee2f
kaylanhua <kaylahuang@college.harvard.edu>
2023-02-05 20:24:46 -0500

    framework in python

--- design1/client.py
+++ design1/client.py
@@ -1,1 +1,39 @@
 # main that sets up and loops
+# You should use sockets and transfer buffers (of your definition) between the machines.
+# TODO: set up a conda env
+
+# send user id, requests need to be sent to user ids
+
+import socket
+HOST = '127.0.0.1'
+PORT = 6000
+
+users = set()
+
+# ==================================
+
+def main() -> None:
+
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((HOST, PORT))
+    
+    msg = 'Hello, World!' # TODO: get from terminal
+    bmsg = msg.encode('ascii')
+    sent = clientsocket.send(bmsg)
+    print(f'Message sent, {sent}/{len(msg)} bytes transmitted')
+    clientsocket.close()
+
+    return
+
+def send_msg(self, addr, line):
+    line = line +'\n'
+    
+    for session in self.sessions:
+        if addr == session.addr:
+            session.push(line.encode())
+            
+            
+    
+if __name__ == "__main__":
+    main()
+
--- design1/server.py
+++ design1/server.py
@@ -1,1 +1,47 @@
 # server side is also a main
+# modulize the connection
+
+import socket
+import thread
+HOST = '127.0.0.1'
+PORT = 6000
+
+# ==================================
+
+# def connections():
+#     print('Got connection from', addr)
+#     while True:
+#         c, addr = s.accept()     # Establish connection with client.
+#         thread.start_new_thread(on_new_client,(c,addr))
+        
+
+# def receive_message():
+    # receives from the client
+    # within each thread function it's just a while loop that listens to a client message
+    
+    
+
+def main() -> None:
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((HOST, PORT))
+    serversocket.listen()
+    clientsocket, addr = serversocket.accept()
+    print('Connected to by:', addr)
+    
+    data = clientsocket.recv(128)
+    data = data.decode('ascii')
+
+    print(f"The data is: {data}")
+
+    serversocket.close()
+
+
+    return
+
+if __name__ == "__main__":
+    main()
+    
+def broadcast(self, line):
+    for session in self.sessions:
+        line = line + '\n'
+        session.push(line.encode())    
