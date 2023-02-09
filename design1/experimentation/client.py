5f757cf4fdeb26b567e7ebf701fede59432eb73b
kaylanhua <kaylahuang@college.harvard.edu>
2023-02-08 22:27:31 -0500

    threading working somewhat

--- design1/experimentation/client.py
+++ design1/experimentation/client.py
@@ -24,32 +24,41 @@ class Client:
         print('Please choose a username.')
         username = input()
         self.username = username
-        self.send_message('0', username)
-        pass
+        success = self.send_message('0', username)
+        if not success:
+            raise Exception('Account creation failed')
         
     def login(self):
         print('Please enter your username.')
         username = input()
         self.username = username
-        self.send_message('1', username)
-        pass
+        success = self.send_message('1', username)
+        if not success:
+            raise Exception('Login failed')
     
-    def send_message(self, opcode, message, target=None):
-        if target:
-            msg = f'{opcode}{target}%{message}'
-        else:
-            msg = f'{opcode}{message}'
+    def send_message(self, opcode, username, message=None, target=None):
+        msg = f'{opcode}%{username}%{target}%{message}'
+        print(msg)
         bmsg = msg.encode('ascii')
         sent = self.conn.sendall(bmsg)
-        print(f'Message sent, {sent}/{len(msg)} bytes transmitted')
-        pass
+        print(f'Message sent, {len(msg)} bytes transmitted')
+
+        # wait for response
+        data = self.conn.recv(1024).decode('ascii') 
+        status, err_msg = data.split('%')
+        if status == '0':
+            print('Success')
+            return True
+        else:
+            print(f'Error {status}: {err_msg}')
+            return False
     
     def query_message(self):
         print('Who would you like to send a message to?')
         target = input()
         print('What is your message?')
         msg = input()
-        self.send_message(2, msg, target)
+        self.send_message(2, self.username, msg, target)
         pass
     
     def welcome_message(self):
@@ -71,7 +80,7 @@ class Client:
         print('What do you want? 2 for send message, 3 for log out, 4 for delete account :(')
         op = input()
         if op == "2":
-            self.query_message()
+            self.query_message()    
         elif op == "3":
             # logout
             self.send_message('3', self.username)
--- design1/experimentation/server.py
+++ design1/experimentation/server.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import threading
 
 HOST = '127.0.0.1'
 PORT = 6000
@@ -13,22 +14,28 @@ class Server:
         self.port = port
         # to track who is logged in (dictionary w all usernames as key and connection as value)
         self.sessions = dict() 
+        # TODO: queue for messages
 
-    def start(self):
+    def listen(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
         self.socket.listen(5)
         print(f"Server started on {self.host}:{self.port}")
-        
-        connection, address = self.socket.accept()
 
-        cont = self.receive_message(connection)
+        while True:
+            client, address = self.socket.accept()
+            # client.settimeout(60)
+            threading.Thread(target = self.start,args = (client, address)).start()
+    
+    def start(self, connection, address):
+        print(f"New connection from {address}")
+        cont = True
         while cont:
             try:
                 cont = self.receive_message(connection)
                 time.sleep(0.5)
-
             except OSError as e:
+                print('Error:', e)
                 pass 
 
     def create_account(self, username, connection):
@@ -44,35 +51,56 @@ class Server:
         print('Data (raw):', data)
         data = data.decode('ascii') 
         if len(data) == 0:
-            return False
+            return True
         print(f"The data is: {data}")
         
-        opcode = data[:1]
-        meat = data[1:]
+        opcode, username, target, message = data.split('%')
+        print(opcode, username, target, message)
+
+        status = 0
+        err_msg = ''
+        
         if opcode == '0':
-            if meat in self.sessions:
+            if username in self.sessions:
                 # user alr exists, log in
-                self.login(meat, connection)
+                self.login(username, connection)
             else:
                 # user does not exist yet, create new user and log in
-                self.create_account(meat, connection)
+                self.create_account(username, connection)
         elif opcode == '1':
-            self.login(meat, connection)
+            self.login(username, connection)
         elif opcode == '2':
-            target, message = meat.split('%')
-            self.new_message(target, message, connection)
+            if target not in self.sessions.keys():
+                status = 1
+                err_msg = (f"User {target} does not exist")
+                # TODO: consider if not logged in
+            else: 
+                # send message to target
+                self.send_message(target, message)
         elif opcode == '3': # log out
-            self.logout(meat)
+            self.logout(username)
             return False
         elif opcode == '4': # delete account
-            self.sessions.pop(meat)
-            self.logout(meat)
+            self.sessions.pop(username)
+            self.logout(username)
             return False
-        
+        elif opcode == '5': # list all users
+            message = str(self.sessions.keys())
+            self.send_message(username, message)
+            return False
+
+        # Return status code (and error message if applicable)
+        response = f"{status}%{err_msg}"
+        self.send_message(username, response)
+
         return True
     
-    def new_message(self, target, message, connection):
+    def send_message(self, target, message):
+        connection = self.sessions[target]
         print(f'Received the following message for {target}: {message}')
+        msg = message.encode('ascii')
+        connection.sendall(msg)
+        pass
     
     def logout(self, username):
         self.sessions[username] = None
@@ -88,7 +116,7 @@ class Server:
 def main() -> None:
 
     serv = Server(HOST, PORT)
-    serv.start()
+    serv.listen()
     
 
 if __name__ == "__main__":
