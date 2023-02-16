e0f709b126321dae354dc4eded2a16d5349c4db1
kaylanhua <kaylahuang@college.harvard.edu>
2023-02-16 16:43:40 -0500

    protos

---
+++ design1/experimentation/grpc_client.py
@@ -0,0 +1,33 @@
+"""The Python implementation of the GRPC helloworld.Greeter client."""
+
+from __future__ import print_function
+
+import logging
+
+import grpc
+import messages_pb2
+import messages_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    # print("Will try to greet world ...")
+    # with grpc.insecure_channel('localhost:50051') as channel:
+    #     stub = helloworld_pb2_grpc.GreeterStub(channel)
+    #     response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    # print("Greeter client received: " + response.message)
+    
+    print("Client starting, input username:")
+    username = input();
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = messages_pb2_grpc.ServerStub(channel)
+        response = stub.ReceiveMessageFromClient(messages_pb2.MessageToServer(opcode = '0', username = username, target = '', message = ''))
+        
+    print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
