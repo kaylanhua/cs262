2bb06fd2a7ec3747b35f7f2e9e8b0f46531df6da
Tom Cobley <43605582+tomcobley@users.noreply.github.com>
2023-02-21 23:12:18 -0500

    tests

--- design1/experimentation/test_grpc_server.py
+++ design1/experimentation/test_grpc_server.py
@@ -1,6 +1,6 @@
 import unittest
 
-from grpc_server import Server, serve
+from grpc_server import Server
 from grpc_client import Client
 import threading
 import time
@@ -25,15 +25,19 @@ class Test(unittest.TestCase):
         port = next_port
         next_port += 1
 
-        # server = Server()
-        server_thread = threading.Thread(target = serve, args = (SERVER_ADDR, port))
+        server = Server()
+        server_thread = threading.Thread(target = server.start, args = (SERVER_ADDR, port))
         server_thread.start()
-        return server_thread, None, port
+
+        # Wait for server to start
+        time.sleep(0.1)
+        
+        return server_thread, server, port
     
     def add_client(self, username, port):
         client = Client(CLIENT_ADDR, port)
-        client.create_account(username)
-        return client
+        response = client.create_account(username)
+        return client, response
     
     def cleanup(self, clients, server, server_thread):
         for client in clients:
@@ -42,59 +46,88 @@ class Test(unittest.TestCase):
         server_thread.join()
 
     def assert_response_contains(self, data, string):
-        self.assertIn(string, data.decode('ascii'))
+        self.assertIn(string, data.message)
     
     def assert_response_not_contains(self, data, string):
-        self.assertNotIn(string, data.decode('ascii'))
+        self.assertNotIn(string, data.message)
 
-    #         # self.cleanup([client1, client2], server, server_thread)
-    #         print('_______ CLEANUP ______________')
+    def test_server_accepts_one_client(self):
+        with test_lock:
+            print('TEST: test_server_accepts_one_client')
+            server_thread, server, port = self.spawn_server()
+
+            # One client connects
+            client1, response1 = self.add_client(TEST_USERNAME_1, port)
+
+            # Both clients receive welcome messages
+            self.assert_response_contains(response1, "Welcome")
+            self.assert_response_contains(response1, TEST_USERNAME_1)
 
 
     def test_server_accepts_two_clients(self):
-        server_thread, server, port = self.spawn_server()
+        with test_lock:
+            print('TEST: test_server_accepts_two_clients')
+            server_thread, server, port = self.spawn_server()
 
-        # Two clients connect
-        client1 = self.add_client(TEST_USERNAME_1, port)
-        client2 = self.add_client(TEST_USERNAME_2, port)
+            # Two clients connect
+            client1, response1 = self.add_client(TEST_USERNAME_1, port)
+            client2, response2 = self.add_client(TEST_USERNAME_2, port)
 
-        # Both clients receive welcome messages
-        data = client1.conn.recv(DATA_SIZE)
-        self.assert_response_contains(data, "Welcome")
-        self.assert_response_contains(data, TEST_USERNAME_1)
+            # Both clients receive welcome messages
+            self.assert_response_contains(response1, "Welcome")
+            self.assert_response_contains(response1, TEST_USERNAME_1)
 
-        data = client2.conn.recv(DATA_SIZE)
-        self.assert_response_contains(data, "Welcome")
-        self.assert_response_contains(data, TEST_USERNAME_2)
+            self.assert_response_contains(response2, "Welcome")
+            self.assert_response_contains(response2, TEST_USERNAME_2)
 
-        # Both usernames are in a list of all users
-        client1.list_all_users()
-        data = client1.conn.recv(DATA_SIZE)
-        self.assert_response_contains(data, TEST_USERNAME_1)
-        self.assert_response_contains(data, TEST_USERNAME_2)
+            # Both usernames are in a list of all users
+            response = client1.list_all_users()
+            self.assert_response_contains(response, TEST_USERNAME_1)
+            self.assert_response_contains(response, TEST_USERNAME_2)
 
-        # Message is received by client2
-        client1.send_message(2, client1.username, TEST_MESSAGE, client2.username)
-        data = client2.conn.recv(DATA_SIZE)
-        self.assert_response_contains(data, TEST_MESSAGE)
+     
+    def test_clients_can_message_eachother(self):
+        with test_lock:
+            print('TEST: test_clients_can_message_eachother')
+            server_thread, server, port = self.spawn_server()
 
-        # Client 2 logs out successfully
-        client2.logout()
-        time.sleep(1)
-        client1.list_all_users()
-        data = client1.conn.recv(DATA_SIZE)
-        self.assert_response_contains(data, TEST_USERNAME_1)
-        self.assert_response_not_contains(data, TEST_USERNAME_2)
+            # Two clients connect
+            client1, response1 = self.add_client(TEST_USERNAME_1, port)
+            client2, response2 = self.add_client(TEST_USERNAME_2, port)
 
-        # Message is queued and delivered after logout/login
-        client1.send_message(2, client1.username, TEST_MESSAGE + "_queued", client2.username)
+            # Message is received by client2
+            client1.send_message('2', client2.username, TEST_MESSAGE)
+            
+            # Get message from server
+            response = client2.send_message('6')
+            self.assert_response_contains(response, TEST_MESSAGE)
 
-        client2 = self.add_client(TEST_USERNAME_2, port)
 
-        data = client2.conn.recv(DATA_SIZE)
-        # self.assert_response_contains(data, TEST_MESSAGE)
+    def test_logout_handled_correctly(self):
+        with test_lock:
+            print('TEST: test_logout_handled_correctly')
+            server_thread, server, port = self.spawn_server()
 
-        self.cleanup([client1, client2], server, server_thread)
+            # Two clients connect
+            client1, response1 = self.add_client(TEST_USERNAME_1, port)
+            client2, response2 = self.add_client(TEST_USERNAME_2, port)
+
+            # Client 2 logs out successfully
+            server.logout(client2.username)
+            time.sleep(1)
+            client1.list_all_users()
+            data = client1.conn.recv(DATA_SIZE)
+            self.assert_response_contains(data, TEST_USERNAME_1)
+            self.assert_response_not_contains(data, TEST_USERNAME_2)
+
+            # Message is queued and delivered after logout/login
+            client1.send_message(2, client1.username, TEST_MESSAGE + "_queued", client2.username)
+
+            client2 = self.add_client(TEST_USERNAME_2, port)
+
+            data = client2.conn.recv(DATA_SIZE)
+            # self.assert_response_contains(data, TEST_MESSAGE)
+
 
 if __name__ == '__main__':
     unittest.main()
