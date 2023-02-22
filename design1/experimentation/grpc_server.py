52f3051fd95df8943aeb2534473f724fd6d40032
Tom Cobley <43605582+tomcobley@users.noreply.github.com>
2023-02-21 23:06:34 -0500

    Restructure server

--- design1/experimentation/grpc_server.py
+++ design1/experimentation/grpc_server.py
@@ -9,34 +9,45 @@ import messages_pb2_grpc
 
 from threading import Lock 
 
-host = 'localhost'  # replace with 0.0.0.0 to open up to other machines
-port = '50051'
-sessions = dict()   # manages which users are currently logged in, as in the socket server
-messages = dict()   # manages which users have outstanding messages which are yet to be delivered
+HOST = 'localhost'  # replace with 0.0.0.0 to open up to other machines
+PORT = '50051'
 
 server_lock = Lock()    # lock for server mutx
 
-# following four functions taken from clean_server.py
-def create_account(username, connection):
-    sessions[username] = connection
-    print(f"{username} has created an account")
-        
-def login(username, connection):
-    sessions[username] = connection
-    print(f"{username} has logged in")
-    
-def logout(username):
-    sessions[username] = None
-    print(f"{username} has logged out")
-
-def queue_message(sender, recipient, message):
-    if recipient not in messages:
-        messages[recipient] = []
-    messages[recipient].append((sender, message))
-
-
 class Server(messages_pb2_grpc.ServerServicer):
 
+    def __init__(self):
+        self.sessions = dict()   # manages which users are currently logged in, as in the socket server
+        self.messages = dict()   # manages which users have outstanding messages which are yet to be delivered
+
+        
+    # following four functions taken from clean_server.py
+    def create_account(self, username, connection):
+        self.sessions[username] = connection
+        print(f"{username} has created an account")
+            
+    def login(self, username, connection):
+        self.sessions[username] = connection
+        print(f"{username} has logged in")
+        
+    def logout(self, username):
+        self.sessions[username] = None
+        print(f"{username} has logged out")
+    
+    def queue_message(self, sender, recipient, message):
+        if recipient not in self.messages:
+            self.messages[recipient] = []
+        self.messages[recipient].append((sender, message))
+
+    def start(self, host, port):
+        # basic gRPC server set up using info from the auto generated messages_pb2_grpc
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        messages_pb2_grpc.add_ServerServicer_to_server(self, server)
+        server.add_insecure_port(host + ':' + str(port)) 
+        server.start()
+        print("Server started, listening on " + str(port))
+        server.wait_for_termination()
+        
     def send_pending(self, messages, username, justLoggedIn=False):
         '''
         sends outstanding messages to the client who has just queried the server
@@ -68,90 +79,81 @@ class Server(messages_pb2_grpc.ServerServicer):
             
             # CREATE ACCOUNT
             if opcode == '0':
-                if username in sessions:
-                    # user already exists, log in
-                    login(username, True)
-                    toClient = f"User already exists. Welcome back, {username}.\n"
-                    if sessions[username] is not None:
-                        # someone else is logged into the requested account
+                if username in self.sessions:
+                    if self.sessions[username] is not None:
+                        # someone else is logged into the requested account, log existing user out
                         toClient = "Someone else has logged into this account so you're being logged out. Goodbye!"
-                        logout(username)
+                        self.logout(username)
+                    # log new user in
+                    self.login(username, True)
+                    toClient = f"User already exists. Welcome back, {username}.\n"
                 else:
                     # user does not exist yet, create new user and log in
-                    create_account(username, True)
+                    self.create_account(username, True)
                     toClient = f"Welcome to your new account, {username}."
 
             # LOG IN
             elif opcode == '1': # log in
-                if username not in sessions:
+                if username not in self.sessions:
                     # user does not exist, give error
                     toClient = "Account does not exist. Please create an account first."
-                    logout(username)
+                    self.logout(username)
                     return messages_pb2.ServerLog(message=toClient)
 
-                if sessions[username] is not None:
+                if self.sessions[username] is not None:
                     # someone else is logged into the requested account
                     toClient = "Someone else has logged into this account so you're being logged out. Goodbye!"
-                    logout(username)
+                    self.logout(username)
                     return messages_pb2.ServerLog(message=toClient)
                 
-                login(username, True)
+                self.login(username, True)
                 toClient = f"Welcome back, {username}.\n"
                 # Send undelivered messages, if any
-                pending = self.send_pending(messages, username, True)
+                pending = self.send_pending(self.messages, username, True)
                 if pending:
                     toClient += pending
 
             # SEND MESSAGE    
             elif opcode == '2':
-                if target not in sessions.keys():
+                if target not in self.sessions.keys():
                     # TODO: raise exception instead?
                     toClient = f"Error: User {target} does not exist. Message could not be sent"
                 else: 
-                    if sessions[target] == None: 
+                    if self.sessions[target] == None: 
                         # target is not logged in, queue message
-                        queue_message(username, target, message)
+                        self.queue_message(username, target, message)
                     else:
                         # send message to target
                         print(f"someone is trying to send a message to {target}")
-                        queue_message(username, target, message)
+                        self.queue_message(username, target, message)
             
             # LOG OUT            
             elif opcode == '3': 
                 toClient = "You have logged out. Goodbye!"
-                logout(username)
+                self.logout(username)
 
             # DELETE ACCOUNT    
             elif opcode == '4':
                 toClient = "Your account has been deleted. Goodbye!"
-                sessions.pop(username)
-                if username in messages:
-                    messages.pop(username)
+                self.sessions.pop(username)
+                if username in self.messages:
+                    self.messages.pop(username)
             
             # LIST ALL USERS    
             elif opcode == '5':
-                toClient = "[ALL ACCOUNTS]" + str([key for key in sessions.keys() if sessions[key] is not None and sessions[key].fileno() != -1])
+                toClient = "[ALL ACCOUNTS]" + str([key for key in self.sessions.keys() if self.sessions[key] is not None])
             
             # QUERYING FOR MESSAGES
             # this is never explicitly chosen by users, this code is used by the client to automatically listen for incoming messages
             elif opcode == '6':
-                res = self.send_pending(messages, username)
+                res = self.send_pending(self.messages, username)
                 if res:
                     toClient = res
 
             return messages_pb2.ServerLog(message=toClient)
-    
-
-def serve(host, port):
-    # basic gRPC server set up using info from the auto generated messages_pb2_grpc
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    messages_pb2_grpc.add_ServerServicer_to_server(Server(), server)
-    server.add_insecure_port(host + ':' + str(port)) 
-    server.start()
-    print("Server started, listening on " + str(port))
-    server.wait_for_termination()
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    serve(host, port)
+    server = Server()
+    server.start(HOST, PORT)
