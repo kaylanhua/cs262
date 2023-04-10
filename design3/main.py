762efb1bfb1f6206f627422e16e0b1178c147cb3
kaylanhua <kaylahuang@college.harvard.edu>
2023-04-09 22:11:59 -0400

    main.py

--- design3/main.py
+++ design3/main.py
@@ -7,12 +7,7 @@ if __name__ == '__main__':
     logging.basicConfig()
     parser = argparse.ArgumentParser()
     parser.add_argument('--id')
-    parser.add_argument('--is_primary', action='store_true')
     args = parser.parse_args()
 
-    if args.is_primary:
-        print('Starting primary server')
-        machine = Server(args.id, args.is_primary)
-    else:
-        machine = Server(args.id, is_primary=False)
+    machine = Server(args.id)
     machine.start(HOST, PORTS[args.id])
