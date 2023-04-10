53c533d881e407d98844f69be6d64c830083c929
kaylanhua <kaylahuang@college.harvard.edu>
2023-04-09 23:55:07 -0400

    ugh:

---
+++ design3/krun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Kill previous run
+pkill -f main.py
+
+# Run servers
+ttab -w 'conda activate 262 ; python main.py --id=A' &
+ttab -w 'conda activate 262 ; python main.py --id=B' &
+ttab -w 'conda activate 262 ; python main.py --id=C' &
+ttab -w 'conda activate 262 ; python main.py --id=D' &
+ttab -w 'conda activate 262 ; python main.py --id=E' &
+
+sleep 1
+
+# Run clients
+ttab -w 'conda activate 262 ; python grpc_client.py' &
+ttab -w 'conda activate 262 ; python grpc_client.py' &
